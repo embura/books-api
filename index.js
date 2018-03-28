3a5b4f48676a43a311797eb78e942012406d01ed
Jefferson da Silva Coelho <jcoelho@igcorp.com.br>
2018-03-28 14:22:30 -0300

    Adicionado workers

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
+const throng = require('throng');
 const app = require('./app');
 
-app.listen('7000', app.get('port'), () => {
-  console.log(`app running on port ${app.get('port')}`);
-});
+throng(2, startWorkers);
+
+async function startWorkers(){
+	await app.listen('7000', app.get('port'), () => {
+  		console.log(`app running on port ${app.get('port')}`);
+	});
+}
